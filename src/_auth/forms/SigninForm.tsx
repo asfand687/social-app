fb937e9f9f7cf3a080dbfae1b24a8fd577a22670
Asfand Yar <asfand@novoassociates.com>
2023-10-29 01:21:35 +0500

    initial app setup and signup page

---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const SigninForm = () => {
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default SigninForm
